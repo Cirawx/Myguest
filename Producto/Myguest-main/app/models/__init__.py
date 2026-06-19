6a91676859afea54dea9e3c2ba55a68eceac47fa
ClaudioMolina27 <cl.molina@duocuc.cl>
2026-06-19 14:45:14 -0400

    Fix en imports de init al separar los modulos

--- Producto/Myguest-main/app/models/__init__.py
+++ Producto/Myguest-main/app/models/__init__.py
@@ -4,4 +4,5 @@ from app.models.proveedor_model import Proveedor, FamiliaProducto
 from app.models.academico_model import Carrera, Asignatura, Taller, PeriodoAcadem, ProgAsign, ProgTaller, RegisTaller, DetRegisTaller
 from app.models.facturacion_model import Factura, DetFactura
 from app.models.compras_model import OrdenCompra, DetOrdenCompra
-from app.models.devolucion_model import Devolucion, DetDevolucion, MotivoMerma, Merma
+from app.models.devolucion_model import Devolucion, DetDevolucion
+from app.models.merma_model import MotivoMerma, Merma
